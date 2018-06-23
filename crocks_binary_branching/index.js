023382b67206a6869f5982f8461493edd637ffce
boygao1992 <wg2313@columbia.edu>
2018-06-23 10:44:38 -0400

    refactor

--- crocks_binary_branching/index.js
+++ crocks_binary_branching/index.js
@@ -17,6 +17,7 @@ const {
   constant,
   curry,
   flip,
+  identity,
   // Combinator
   compose,
   // Point-free
@@ -90,36 +91,48 @@ const average_5 = converge(
 )
 
 /* solution 6: traverse a List of Readers */
-const operations = List( [
-  sum,
-  length
-] )
 
-// average_6 :: Reader [ Number ] Number
-const average_6 = compose(
-  map( ( [ x, y ] ) => divide( x, y ) ),
-  map( listToArray ),
-  sequence( Reader.of ),
-  map( ask ),
-  constant( operations )
-)
+// blueprint:
+// List(operations) // List Func
+// .map( ask ) // List (Reader Number Number)
+// .sequence( Reader.of ) // Reader Number (List Number)
+// .map( listToArray ) // Reader Number [ Number ]
+// .map( ( [ x, y ] ) => divide( x, y ) ) // Reader Number Number
+// .runWith( data ) // Number
 
 // converge :: ((x1, x2, …) → z) → [((a, b, …) → x1), ((a, b, …) → x2), …] → (a → b → … → z)
 // myConverge :: ((x1, x2, …) → b) → [(a → x1), (a → x2), …] → Reader a b
-const myConverge = reducer => compose(
-  map( reducer ),
-  map( listToArray ),
-  sequence( Reader.of ),
-  map( ask ),
+const myConverge = curry(
+  reducer => compose(
+    map( reducer ),
+    map( listToArray ),
+    sequence( Reader.of ),
+    map( ask ),
+    List,
+  )
 )
 
+const operations = [
+  sum,
+  length
+]
+
+// dummyReducer :: [ Number, Number ] -> Number
+const dummyReducer = ( [ x, y ] ) => divide( x, y )
+
+// average_6 :: Reader [ Number ] Number
+const average_6 = myConverge( dummyReducer, operations )
+
+// 0 -> [ 1, 2, 3, 4]
 // log(
-//   operations // List Func
-//   .map( ask ) // List (Reader Number Number)
-//   .sequence( Reader.of ) // Reader Number (List Number)
-//   .map( listToArray ) // Reader Number [ Number ]
-//   .map( ( [ x, y ] ) => divide( x, y ) ) // Reader Number Number
-//   .runWith( data ) // Number
+//   runWith( 0 )(
+//     myConverge( identity )( [
+//       x => x + 1,
+//       x => x + 2,
+//       x => x + 3,
+//       x => x + 4
+//     ] )
+//   )
 // )
 
 log( [
@@ -128,5 +141,5 @@ log( [
   average_3( data ),
   average_4( data ),
   average_5( data ),
-  runWith( data )( average_6() )
+  runWith( data )( average_6 )
 ] )
