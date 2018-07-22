18f251ed1f96e91e1345e7598394b27559346721
boygao1992 <wg2313@columbia.edu>
2018-07-22 19:23:39 -0400

    elm_recursive_update: basic model done, able to generate view based on a list of components; need to design internal message model

--- elm_recursive_update/Main.elm
+++ elm_recursive_update/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (..)
 import Dict
 import Html as H
 import Html.Attributes as A
+import Html.Events as E
 
 
 -- Component decorator to distinguish different instances of the same type
@@ -13,11 +14,28 @@ type alias Id =
 
 
 type alias WithId a =
-    { a | id : Id }
+    { id : Id
+    , value : a
+    }
+
+
+getId : WithId a -> Id
+getId { id } =
+    id
+
+
+getValue : WithId a -> a
+getValue { value } =
+    value
+
+
+withId : Id -> a -> WithId a
+withId id a =
+    { id = id, value = a }
 
 
 
--- Component 1
+-- Component 1: Button
 
 
 type alias ButtonModel =
@@ -28,8 +46,24 @@ type ButtonMsg
     = Pressed
 
 
+buttonUpdate : ButtonMsg -> ButtonModel -> ( ButtonModel, Cmd msg )
+buttonUpdate msg model =
+    case msg of
+        Pressed ->
+            ( { model | active = not model.active }, Cmd.none )
 
--- Component 2
+
+buttonView : String -> ButtonModel -> H.Html ButtonMsg
+buttonView label model =
+    H.button
+        [ A.disabled (model.active == False)
+        , E.onClick Pressed
+        ]
+        [ H.text label ]
+
+
+
+-- Component 2: InputField
 
 
 type alias InputFieldModel =
@@ -38,6 +72,22 @@ type alias InputFieldModel =
 
 type InputFieldMsg
     = KeyInput String
+
+
+inputFieldUpdate : InputFieldMsg -> InputFieldModel -> ( InputFieldModel, Cmd msg )
+inputFieldUpdate msg model =
+    case msg of
+        KeyInput s ->
+            ( { model | input = s }, Cmd.none )
+
+
+inputFieldView : String -> InputFieldModel -> H.Html InputFieldMsg
+inputFieldView default model =
+    H.input
+        [ A.placeholder default
+        , E.onInput KeyInput
+        ]
+        []
 
 
 
@@ -80,8 +130,20 @@ type ChildMsg
     | ChildMsgInputField (WithId InputFieldMsg)
 
 
+childModelView : ChildModel -> H.Html ChildMsg
+childModelView child =
+    case child of
+        ChildModelButton b ->
+            buttonView (getId b) (getValue b)
+                |> H.map (ChildMsgButton << (withId (getId b)))
 
--- All Constraint together
+        ChildModelInputField i ->
+            inputFieldView (getId i) (getValue i)
+                |> H.map (ChildMsgInputField << (withId (getId i)))
+
+
+
+-- All Constraints together
 
 
 type alias ConstraintModel =
@@ -99,8 +161,13 @@ type ConstraintMsg
 -- Container
 
 
+type ComponentModel
+    = Widget ChildModel
+    | Constraint ConstraintModel
+
+
 type alias Model =
-    { children : List ChildModel }
+    List ComponentModel
 
 
 type Msg
@@ -110,12 +177,38 @@ type Msg
 
 initialModel : Model
 initialModel =
-    {}
+    [ Widget <| ChildModelButton <| withId "1" <| { active = True }
+    , Widget <| ChildModelButton <| withId "2" <| { active = False }
+    , Widget <| ChildModelButton <| withId "3" <| { active = False }
+    , Widget <| ChildModelInputField <| withId "4" <| { input = "" }
+    , Widget <| ChildModelInputField <| withId "5" <| { input = "" }
+    ]
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
+update _ model =
+    ( model, Cmd.none )
+
+
+dispatchView : ComponentModel -> H.Html Msg
+dispatchView component =
+    case component of
+        Widget child ->
+            childModelView child
+                |> H.map MsgChild
+
+        Constraint _ ->
+            H.text ""
+
+
+view : Model -> H.Html Msg
+view model =
+    H.div [] (model |> List.map dispatchView)
 
 
 main : Program Never Model Msg
 main =
-    Html.program
+    H.program
         { init = ( initialModel, Cmd.none )
         , update = update
         , subscriptions = \model -> Sub.none
