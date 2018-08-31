ab06d0d0d38b62934332d4d22e782c1930d73825
boygao1992 <wg2313@columbia.edu>
2018-08-31 01:00:10 -0400

    purescript_halogen_starter: halogen's API has changed since the last update; ComponentSpec no longer needs initializer and finializer, which are moved to LifecycleComponentSpec; ComponentHTML replaces direct HTML, which only needs the Query functor algebra

---
+++ purescript_halogen_starter/src/Main.purs
@@ -0,0 +1,63 @@
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+-- import Effect.Console (log)
+
+import Data.Const (Const)
+import Data.Maybe (Maybe(..))
+
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import Halogen.VDom.Driver (runUI)
+
+type State = Boolean
+
+data Query next -- Input Events, Functor Algebra for Free Monad
+  = Toggle next
+  | IsOn (Boolean -> next)
+
+data Message = Toggled Boolean -- Output Events
+
+myButton :: forall m. H.Component HH.HTML Query Unit Message m
+myButton =
+  H.component
+    { initialState : const initialState
+    , render
+    , eval
+    , receiver : const Nothing
+    }
+  where
+    initialState :: State
+    initialState = false
+
+    render :: State -> H.ComponentHTML Query
+    render state =
+      let
+        label = if state then "On" else "Off" -- translator from data model to view model
+      in
+        HH.button
+          [ HP.title label
+          , HE.onClick (HE.input_ Toggle)
+          ]
+          [ HH.text label ]
+
+    eval :: Query ~> H.HalogenM State Query (Const Void) Void Message m
+    eval (Toggle next) = do
+      state <- H.get
+      let nextState = not state
+      H.put nextState
+      H.raise $ Toggled nextState
+      pure next
+    eval (IsOn reply) = do
+      state <- H.get
+      pure (reply state)
+
+main :: Effect Unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI myButton unit body
