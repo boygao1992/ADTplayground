1d548d6c4b9aa6b80d17e5ade455e5060305eff8
boygao1992 <wg2313@columbia.edu>
2018-09-15 02:06:24 -0400

    purescript_aff: better abstraction than Promise and Async|Await; now understand how purescript-halogen-select's primitive debouncer works; need more experiments on its Applicative ParAff

--- purescript_avar/src/Main.purs
+++ purescript_avar/src/Main.purs
@@ -1,13 +1,37 @@
 module Main where
 
 import Prelude
+
+-- import AVar as AVar
 import Effect (Effect)
+import Effect.Class (liftEffect)
+import Effect.Aff (Aff)
+import Effect.Aff as Aff
+import Effect.Aff.AVar as AVar
+import Effect.Aff.Class (liftAff)
 import Effect.Console (log)
-import AVar as AVar
 
 main :: Effect Unit
 main = do
-  var <- AVar.new "foo"
-  val1 <- AVar.tryRead var
-  val2 <- AVar.tryRead var
-  log $ show val1 <> show val2
+  Aff.launchAff_ do
+    var <- AVar.empty
+
+    _ <- Aff.forkAff do
+      i <- AVar.take var
+      liftEffect $ log $ "take " <> show i
+
+    _ <- Aff.forkAff do
+      i <- AVar.take var
+      liftEffect $ log $ "take " <> show i
+
+    _ <- Aff.forkAff do
+      Aff.delay $ Aff.Milliseconds 1000.0
+      AVar.put 1 var
+      liftEffect $ log $ "put 1"
+
+    _ <- Aff.forkAff do
+      Aff.delay $ Aff.Milliseconds 2000.0
+      AVar.put 2 var
+      liftEffect $ log $ "put 2"
+
+    pure unit
