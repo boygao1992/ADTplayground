57674edbbe4e12d3f9f9d5982913d0b32f9e08fe
boygao1992 <wg2313@columbia.edu>
2018-10-18 17:15:39 -0400

    js_class_project

--- js_class_projects/neo4j/src/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  WEB_ENDPOINT: 'http://ec2-54-145-166-247.compute-1.amazonaws.com:7474',
-  BOLT_ENDPOINT: 'bolt://ec2-54-145-166-247.compute-1.amazonaws.com:7687',
-  USER_NAME: 'neo4j',
-  USER_PWD: 'FkQQqhNG1gVrshmdVab1',
-  DYNAMODB_TABLENAME: 'userFriends'
-}
