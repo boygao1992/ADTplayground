600ca653c6c52fe51017db05231e6b77c5944659
boygao1992 <wg2313@columbia.edu>
2018-12-09 20:30:32 -0500

    gin_starter

---
+++ golang_starter/gin_starter/serve-static/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(static.Serve("/", static.LocalFile("./resource", false)))
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8080")
+}
