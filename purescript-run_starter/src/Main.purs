099c6e02efd549589916ef661f3b71749e66958c
boygao1992 <wg2313@columbia.edu>
2018-12-13 03:33:07 -0500

    purescript-run: exception effect

--- purescript-run_starter/src/Main.purs
+++ purescript-run_starter/src/Main.purs
@@ -1,10 +1,12 @@
 module Main where
 
 import Prelude
+
 import Effect (Effect)
 import Effect.Console (log, logShow)
-
 import Run (EFFECT, Run, liftEffect, runBaseEffect)
+import Run.Except (EXCEPT, FAIL)
+import Run.Except (throw, catch) as Except
 import Run.State (STATE)
 import Run.State (get, gets, modify, put, putAt, runState, runStateAt) as State
 
@@ -20,18 +22,26 @@ initalization :: forall r. Run ( state :: STATE AppState | r) Unit
 initalization = do
   State.put { count : 0 }
 
-program :: forall r. Run ( state :: STATE AppState, effect :: EFFECT | r) Unit
-program = do
+processor :: forall r. Run ( state :: STATE AppState, effect :: EFFECT | r) Unit
+processor = do
   state <- State.get
   liftEffect $ log $ "current state: " <> show state
   State.modify _ { count = state.count + 1 }
 
+validator :: forall r. Run ( state :: STATE AppState, except :: EXCEPT String | r) Unit
+validator = do
+  state <- State.get
+  if state.count < 2
+    then Except.throw "too small"
+    else State.put { count: state.count - 1 }
 
 main :: Effect Unit
 main = do
-  result <- runBaseEffect
-    <<< (State.runState { count: 10 })
+        void
+    <<< runBaseEffect
+    <<< State.runState { count: 10 }
+    <<< Except.catch (liftEffect <<< logShow)
     $ do
-    initalization
-    program
-  logShow result
+      initalization
+      processor
+      validator
