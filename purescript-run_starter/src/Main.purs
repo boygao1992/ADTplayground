7eb7616e6fe6ea31522c06f37731b6ffa66fb8f9
boygao1992 <wg2313@columbia.edu>
2018-12-13 19:43:46 -0500

    purescript-run_starter: Reader, Writer

--- purescript-run_starter/src/Main.purs
+++ purescript-run_starter/src/Main.purs
@@ -5,43 +5,67 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log, logShow)
 import Run (EFFECT, Run, liftEffect, runBaseEffect)
-import Run.Except (EXCEPT, FAIL)
+import Run.Except (EXCEPT)
 import Run.Except (throw, catch) as Except
 import Run.State (STATE)
-import Run.State (get, gets, modify, put, putAt, runState, runStateAt) as State
+import Run.State (get, modify, put, runState) as State
+import Run.Reader (READER)
+import Run.Reader (ask, runReader) as Reader
+import Run.Writer (WRITER)
+import Run.Writer (tell, runWriter) as Writer
+
+type AppConfig =
+  { seed :: Int }
 
 type AppState =
-  { count :: Int }
+  { count :: Int
+  , step :: Int
+  }
 
 type AppEffects =
   ( state :: STATE AppState
+  , reader :: READER AppConfig
   , effect :: EFFECT
   )
 
-initalization :: forall r. Run ( state :: STATE AppState | r) Unit
-initalization = do
-  State.put { count : 0 }
+type AppLog = Array String
 
-processor :: forall r. Run ( state :: STATE AppState, effect :: EFFECT | r) Unit
+initalization :: forall r. Run ( state :: STATE AppState, reader :: READER AppConfig, writer :: WRITER AppLog | r) Unit
+initalization = do
+  env <- Reader.ask
+  State.put { count : env.seed * 10, step : 1 }
+  Writer.tell [ "initialization: success" ]
+
+processor :: forall r. Run ( state :: STATE AppState, effect :: EFFECT, writer :: WRITER AppLog | r) Unit
 processor = do
   state <- State.get
   liftEffect $ log $ "current state: " <> show state
   State.modify _ { count = state.count + 1 }
+  State.modify _ { step = state.step + 1 }
+  Writer.tell [ "processor: success" ]
 
-validator :: forall r. Run ( state :: STATE AppState, except :: EXCEPT String | r) Unit
+validator :: forall r. Run ( state :: STATE AppState, except :: EXCEPT String, writer :: WRITER AppLog | r) Unit
 validator = do
   state <- State.get
+  State.modify _ { step = state.step + 1 }
   if state.count < 2
-    then Except.throw "too small"
-    else State.put { count: state.count - 1 }
+    then do
+      Writer.tell [ "validator: failure" ]
+      Except.throw "too small"
+    else do
+      State.modify _ { count = state.count - 1 }
+      Writer.tell [ "validator: success" ]
 
 main :: Effect Unit
 main = do
-        void
-    <<< runBaseEffect
-    <<< State.runState { count: 10 }
+  result <- runBaseEffect
+    <<< State.runState { count: 0, step: 0 }
+    <<< Reader.runReader { seed: 0 }
+    <<< Writer.runWriter
     <<< Except.catch (liftEffect <<< logShow)
-    $ do
-      initalization
-      processor
-      validator
+      $ do
+          initalization
+          processor
+          validator
+
+  logShow result
