db1888fc8348281743ec9b4aaf7c20fc480d23cd
boygao1992 <wg2313@columbia.edu>
2019-01-08 16:56:19 -0500

    purescript-graphql: EnumReadSymbol, map from a name to a data constructor in an Enum

--- purescript-graphql_enhance/src/Generic/EnumToArray.purs
+++ purescript-graphql_enhance/src/Generic/EnumToArray.purs
@@ -3,11 +3,13 @@ module EnumToArray where
 import Data.Generic.Rep
 import Prelude
 
+import Control.Alt ((<|>))
+import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import GraphQL.Type (EnumValue, enumValue)
-import Type.Proxy (Proxy)
 import Type.Data.Symbol (SProxy(..))
 import Type.Data.Symbol as Symbol
+import Type.Proxy (Proxy)
 
 -- assumption: IsEnum rep =>
 -- class EnumToArray a rep | a -> rep where
@@ -25,3 +27,44 @@ import Type.Data.Symbol as Symbol
 -- else instance enumToArrayInductionStep ::
 --   (
 --   ) => EnumToArray a ()
+
+enumReadSymbol :: forall a rep
+   . Generic a rep
+  => EnumReadSymbol rep
+  => String
+  -> Either String a
+enumReadSymbol = map to <<< enumReadSymbolImpl
+
+class EnumReadSymbol rep where
+  enumReadSymbolImpl :: String -> Either String rep
+
+instance enumReadSymbolBaseCase ::
+  ( Symbol.IsSymbol name2
+  ) => EnumReadSymbol (Constructor name2 NoArguments)
+  where
+    enumReadSymbolImpl name1 =
+      if (name1 == name2)
+      then
+        Right $ Constructor NoArguments
+      else
+        Left $ name1 <> " doesn't match " <> name2
+
+      where
+        name2 = (Symbol.reflectSymbol (SProxy :: SProxy name2))
+else instance enumReadSymbolInductionStep ::
+  ( EnumReadSymbol a
+  , EnumReadSymbol b
+  ) => EnumReadSymbol (Sum a b)
+  where
+    enumReadSymbolImpl name = Inl <$> enumReadSymbolImpl name
+                          <|> Inr <$> enumReadSymbolImpl name
+
+-- | Test
+data Action
+  = Create
+  | Read
+  | Update
+  | Delete
+derive instance genericAction :: Generic Action _
+
+example1 = (enumReadSymbol "Create") :: Either String Action
