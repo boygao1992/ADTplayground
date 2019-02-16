c9956a58b9ed157778f5b4d26a94f365f9dd1cbb
boygao1992 <wg2313@columbia.edu>
2019-02-16 18:16:51 -0500

    purescript-frp_miniyampa: effectful evaluation primitives

--- purescript_frp/src/FRP/MiniYampa/Types.purs
+++ purescript_frp/src/FRP/MiniYampa/Types.purs
@@ -2,13 +2,24 @@ module FRP.MiniYampa.Types where
 
 import Prelude
 
+import Control.Monad.Rec.Class (Step (..), tailRecM3)
+import Data.Maybe (Maybe (..), fromMaybe)
 import Data.Profunctor (class Profunctor)
 import Data.Profunctor.Strong (class Strong)
 import Data.Tuple (Tuple(..))
+import Effect (Effect)
+import Effect.Ref (Ref)
+import Effect.Ref as Ref
+
+-- | Time
 
 type Time = Number
 type DTime = Number
-data Event a = Event a | NoEvent
+
+-- | Event
+type Event = Maybe
+
+-- | Signal Function (SF)
 newtype SF a b = SF (DTime -> a -> { sf :: SF a b, output :: b })
 
 instance semigroupoidSF :: Semigroupoid SF where
@@ -64,9 +75,82 @@ instance strongSF :: Strong SF where
         in
           { sf: g, output: Tuple c b }
 
-
--- instance profunctor :: Profunctor
-
 runSF :: forall a b. SF a b -> DTime -> a -> { sf :: SF a b, output :: b }
 runSF (SF sf) = sf
 
+-- | Effectful Evaluation
+
+newtype ReactState a b = ReactState
+  { actuate :: Ref (ReactState a b) -> Boolean -> b -> Effect Boolean
+  , sf :: SF a b
+  , a :: a
+  , b :: b
+  }
+
+reactInit
+  :: forall a b
+   . Effect a
+  -> (Ref (ReactState a b)  -> Boolean -> b -> Effect Boolean)
+  -> SF a b
+  -> Effect (Ref (ReactState a b))
+reactInit init actuate (SF a2b) = do
+  a <- init
+  let a2br = a2b 0.0 a -- first time interval of duration 0.0
+      a2b' = a2br.sf
+      b = a2br.output
+  stateRef <- Ref.new (ReactState { actuate, sf: a2b', a, b })
+  void $ actuate stateRef true b
+  pure stateRef
+
+react
+  :: forall a b
+   . Ref (ReactState a b)
+  -> Tuple DTime (Maybe a)
+  -> Effect Boolean
+react rh (Tuple dt ma') = do
+  (ReactState rs) <- Ref.read rh
+  let a' = fromMaybe rs.a ma'
+      r = (runSF rs.sf) dt a'
+  Ref.write
+    (ReactState rs { sf = r.sf, a = a', b = r.output })
+    rh
+  rs.actuate rh true r.output
+
+reactimate
+  :: forall a b
+   . Effect a
+  -> (Boolean -> Effect (Tuple DTime (Maybe a)))
+  -> (Boolean -> b -> Effect Boolean)
+  -> SF a b
+  -> Effect Unit
+reactimate init sense actuate (SF sf0) = do
+  a0 <- init
+  let r = sf0 0.0 a0
+  tailRecM3 go r.sf a0 r.output
+    where
+      go (SF sf) a b = do
+        done <- actuate true b
+        if done
+          then pure $ Done unit
+          else do
+            (Tuple dt ma') <- sense false
+            let a' = fromMaybe a ma'
+                r2 = sf dt a'
+            pure $ Loop { a: r2.sf, b: a', c: r2.output }
+
+-- | General signal functions
+edge :: SF Boolean (Event Unit)
+edge = SF (b2e true)
+  where
+    b2e
+      :: Boolean
+      -> DTime -> Boolean -> { sf :: SF Boolean (Event Unit)
+                             , output :: Event Unit
+                             }
+    b2e prev dt curr =
+      { sf : SF (b2e curr)
+      , output:
+        if not prev && curr
+        then Just unit
+        else Nothing
+      }
