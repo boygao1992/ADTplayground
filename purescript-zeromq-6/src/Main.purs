3c341bfb0a0a44e393ec97ce2fb3575bec83e97a
boygao1992 <wg2313@columbia.edu>
2020-02-09 23:58:26 -0500

    add push-pull example

--- purescript-zeromq-6/src/Main.purs
+++ purescript-zeromq-6/src/Main.purs
@@ -13,17 +13,21 @@ import ZeroMQ.Internal (Address(..))
 import ZeroMQ.Internal as ZeroMQ.Internal
 
 main :: Effect Unit
-main = do
-  runPublisher
-  runSubscriber
+main = pushPullExample
 
 address = Address "tcp://127.0.0.1:3000" :: Address
+
+pubSubExample :: Effect Unit
+pubSubExample = do
+  runPublisher
+  runSubscriber
 
 runPublisher :: Effect Unit
 runPublisher = do
   socket <- ZeroMQ.Internal.newPublisher
   Effect.Aff.launchAff_ do
     ZeroMQ.Internal.bind socket address
+    Effect.Console.log "Publisher bound to port 3000"
     forever do
       Effect.Console.log "sending a multipart message envelope"
       ZeroMQ.Internal.sendMany socket
@@ -36,7 +40,47 @@ runSubscriber :: Effect Unit
 runSubscriber = do
   socket <- ZeroMQ.Internal.newSubscriber
   ZeroMQ.Internal.connect socket address
+  Effect.Console.log "Subscriber connected to port 3000"
   ZeroMQ.Internal.subscribeAll socket
+  Effect.Aff.launchAff_
+    $ tailRecM
+        ( \mx -> do
+            case mx of
+              Nothing -> pure unit
+              Just xs ->
+                Effect.Console.logShow
+                  <<< map (Node.Buffer.Immutable.toString UTF8)
+                  $ xs
+            closed <- liftEffect $ ZeroMQ.Internal.closed socket
+            if closed then
+              pure $ Done unit
+            else
+              Loop <<< Just <$> ZeroMQ.Internal.receive socket
+        )
+        Nothing
+
+pushPullExample :: Effect Unit
+pushPullExample = do
+  runProducer
+  runWorker
+
+runProducer :: Effect Unit
+runProducer = do
+  socket <- ZeroMQ.Internal.newPush
+  Effect.Aff.launchAff_ do
+    ZeroMQ.Internal.bind socket address
+    Effect.Console.log "Producer bound to port 3000"
+    forever do
+      ZeroMQ.Internal.sendMany socket
+        [ Node.Buffer.Immutable.fromString "some work" UTF8 ]
+      Effect.Console.log "work sent"
+      Effect.Aff.delay (Effect.Aff.Milliseconds 500.0)
+
+runWorker :: Effect Unit
+runWorker = do
+  socket <- ZeroMQ.Internal.newPull
+  ZeroMQ.Internal.connect socket address
+  Effect.Console.log "Worker connected to port 3000"
   Effect.Aff.launchAff_
     $ tailRecM
         ( \mx -> do
