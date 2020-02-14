d205bccfe8d1ed1ec6ec3d4e25b52ea23259107a
boygao1992 <wg2313@columbia.edu>
2020-02-13 20:25:35 -0500

    use Coroutine instead of low-level tailRecM
    other viable stream programming abstractions:
    1. haskell-pipes (ListT done right)
    2. haskell-streaming
    3. purescript-machines

--- purescript-zeromq-6/src/Main.purs
+++ purescript-zeromq-6/src/Main.purs
@@ -1,16 +1,22 @@
 module Main where
 
 import Prelude
-import Control.Monad.Rec.Class (Step(..), forever, tailRecM)
+import Control.Coroutine (Producer, Consumer)
+import Control.Coroutine as Control.Coroutine
+import Control.Monad.Rec.Class (forever)
+import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Aff (Aff)
 import Effect.Aff as Effect.Aff
 import Effect.Class (liftEffect)
-import Effect.Class.Console as Effect.Console
+import Effect.Class.Console as Effect.Class.Console
+import Node.Buffer.Immutable (ImmutableBuffer)
 import Node.Buffer.Immutable as Node.Buffer.Immutable
 import Node.Encoding (Encoding(..))
 import ZeroMQ.Internal (Address(..))
 import ZeroMQ.Internal as ZeroMQ.Internal
+import ZeroMQ.Types (Socket)
 
 main :: Effect Unit
 main = pushPullExample
@@ -27,9 +33,9 @@ runPublisher = do
   socket <- ZeroMQ.Internal.newPublisher
   Effect.Aff.launchAff_ do
     ZeroMQ.Internal.bind socket address
-    Effect.Console.log "Publisher bound to port 3000"
+    Effect.Class.Console.log "Publisher bound to port 3000"
     forever do
-      Effect.Console.log "sending a multipart message envelope"
+      Effect.Class.Console.log "sending a multipart message envelope"
       ZeroMQ.Internal.sendMany socket
         [ Node.Buffer.Immutable.fromString "kitty cats" UTF8
         , Node.Buffer.Immutable.fromString "meow!" UTF8
@@ -40,24 +46,11 @@ runSubscriber :: Effect Unit
 runSubscriber = do
   socket <- ZeroMQ.Internal.newSubscriber
   ZeroMQ.Internal.connect socket address
-  Effect.Console.log "Subscriber connected to port 3000"
+  Effect.Class.Console.log "Subscriber connected to port 3000"
   ZeroMQ.Internal.subscribeAll socket
   Effect.Aff.launchAff_
-    $ tailRecM
-        ( \mx -> do
-            case mx of
-              Nothing -> pure unit
-              Just xs ->
-                Effect.Console.logShow
-                  <<< map (Node.Buffer.Immutable.toString UTF8)
-                  $ xs
-            closed <- liftEffect $ ZeroMQ.Internal.closed socket
-            if closed then
-              pure $ Done unit
-            else
-              Loop <<< Just <$> ZeroMQ.Internal.receive socket
-        )
-        Nothing
+    $ Control.Coroutine.runProcess
+    $ Control.Coroutine.connect (producer socket) consumer
 
 pushPullExample :: Effect Unit
 pushPullExample = do
@@ -69,31 +62,37 @@ runProducer = do
   socket <- ZeroMQ.Internal.newPush
   Effect.Aff.launchAff_ do
     ZeroMQ.Internal.bind socket address
-    Effect.Console.log "Producer bound to port 3000"
+    Effect.Class.Console.log "Producer bound to port 3000"
     forever do
       ZeroMQ.Internal.sendMany socket
         [ Node.Buffer.Immutable.fromString "some work" UTF8 ]
-      Effect.Console.log "work sent"
+      Effect.Class.Console.log "work sent"
       Effect.Aff.delay (Effect.Aff.Milliseconds 500.0)
 
 runWorker :: Effect Unit
 runWorker = do
   socket <- ZeroMQ.Internal.newPull
   ZeroMQ.Internal.connect socket address
-  Effect.Console.log "Worker connected to port 3000"
+  Effect.Class.Console.log "Worker connected to port 3000"
   Effect.Aff.launchAff_
-    $ tailRecM
-        ( \mx -> do
-            case mx of
-              Nothing -> pure unit
-              Just xs ->
-                Effect.Console.logShow
-                  <<< map (Node.Buffer.Immutable.toString UTF8)
-                  $ xs
-            closed <- liftEffect $ ZeroMQ.Internal.closed socket
-            if closed then
-              pure $ Done unit
-            else
-              Loop <<< Just <$> ZeroMQ.Internal.receive socket
-        )
-        Nothing
+    $ Control.Coroutine.runProcess
+    $ Control.Coroutine.connect (producer socket) consumer
+
+producer :: forall from. Socket from -> Producer (Array ImmutableBuffer) Aff Unit
+producer socket = Control.Coroutine.producer receiver
+  where
+  receiver :: Aff (Either (Array ImmutableBuffer) Unit)
+  receiver = do
+    closed <- liftEffect $ ZeroMQ.Internal.closed socket
+    if closed then
+      pure $ Right unit
+    else
+      Left <$> ZeroMQ.Internal.receive socket
+
+consumer :: Consumer (Array ImmutableBuffer) Aff Unit
+consumer = Control.Coroutine.consumer emitter
+  where
+  emitter :: Array ImmutableBuffer -> Aff (Maybe Unit)
+  emitter xs = do
+    Effect.Class.Console.logShow <<< map (Node.Buffer.Immutable.toString UTF8) $ xs
+    pure Nothing
