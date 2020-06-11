dfcc0be4ad45e4d847aa0b37a03a4e6d72b024d8
boygao1992 <wg2313@columbia.edu>
2020-06-11 18:33:49 -0400

    add more tests

--- purescript-frp/test/Test/Event.purs
+++ purescript-frp/test/Test/Event.purs
@@ -1,7 +1,10 @@
 module Test.Event (suite) where
 
 import Prelude
+import Control.Alt ((<|>))
+import Control.Plus as Control.Plus
 import Data.Array as Data.Array
+import Data.Traversable as Data.Traversable
 import Effect (Effect)
 import Effect.Class (liftEffect)
 import Effect.Ref as Effect.Ref
@@ -13,14 +16,13 @@ suite :: Test.Unit.TestSuite
 suite =
   Test.Unit.suite "Event" do
     Test.Unit.suite "subscribe" do
-      Test.Unit.test "supports multiple listeners" do
+      Test.Unit.test "supports multiple subscribers" do
         actual <-
           liftEffect do
             s :: FRP.SinkEvent Int <- FRP.sinkEvent
             spy1 <- subscribeSpy s.event
             spy2 <- subscribeSpy s.event
-            s.push 2
-            s.push 3
+            Data.Traversable.for_ [ 0, 1 ] s.push
             { spy1: _
             , spy2: _
             }
@@ -28,19 +30,18 @@ suite =
               <*> spy2.received
         let
           expected =
-            { spy1: [ 2, 3 ]
-            , spy2: [ 2, 3 ]
+            { spy1: [ 0, 1 ]
+            , spy2: [ 0, 1 ]
             }
         Test.Unit.Assert.equal expected actual
-      Test.Unit.test "single listeners can be removed" do
+      Test.Unit.test "single subscriber can be removed" do
         actual <-
           liftEffect do
             s :: FRP.SinkEvent Int <- FRP.sinkEvent
             spy1 <- subscribeSpy s.event
             spy2 <- subscribeSpy s.event
             spy2.unsubscribe
-            s.push 2
-            s.push 3
+            Data.Traversable.for_ [ 0, 1 ] s.push
             { spy1: _
             , spy2: _
             }
@@ -48,11 +49,11 @@ suite =
               <*> spy2.received
         let
           expected =
-            { spy1: [ 2, 3 ]
+            { spy1: [ 0, 1 ]
             , spy2: []
             }
         Test.Unit.Assert.equal expected actual
-      Test.Unit.test "supports removing listener when more than two" do
+      Test.Unit.test "supports removing subscriber when more than two" do
         actual <-
           liftEffect do
             s :: FRP.SinkEvent Int <- FRP.sinkEvent
@@ -60,8 +61,7 @@ suite =
             spy2 <- subscribeSpy s.event
             spy3 <- subscribeSpy s.event
             spy2.unsubscribe
-            s.push 2
-            s.push 3
+            Data.Traversable.for_ [ 0, 1 ] s.push
             { spy1: _
             , spy2: _
             , spy3: _
@@ -71,11 +71,206 @@ suite =
               <*> spy3.received
         let
           expected =
-            { spy1: [ 2, 3 ]
+            { spy1: [ 0, 1 ]
             , spy2: []
-            , spy3: [ 2, 3 ]
+            , spy3: [ 0, 1 ]
             }
         Test.Unit.Assert.equal expected actual
+    Test.Unit.suite "alt (Alt)" do
+      Test.Unit.test "should combine two event streams and preserve time topology" do
+        actual <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            s2 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              combined :: FRP.Event Int
+              combined = s1.event <|> s2.event
+            spy1 <- subscribeSpy combined
+            s1.push 0
+            s2.push 1
+            s2.push 2
+            s1.push 3
+            spy1.received
+        Test.Unit.Assert.equal [ 0, 1, 2, 3 ] actual
+      Test.Unit.test "obeys associative law: (x <|> y) <|> z = x <|> (y <|> z)" do
+        let
+          pulses ::
+            { s1 :: FRP.SinkEvent Int
+            , s2 :: FRP.SinkEvent Int
+            , s3 :: FRP.SinkEvent Int
+            } ->
+            Effect Unit
+          pulses { s1, s2, s3 } = do
+            s1.push 0
+            s2.push 1
+            s3.push 2
+        left <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            s2 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            s3 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              combined :: FRP.Event Int
+              combined = (s1.event <|> s2.event) <|> s3.event
+            spy1 <- subscribeSpy combined
+            pulses { s1, s2, s3 }
+            spy1.received
+        right <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            s2 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            s3 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              combined :: FRP.Event Int
+              combined = s1.event <|> (s2.event <|> s3.event)
+            spy1 <- subscribeSpy combined
+            pulses { s1, s2, s3 }
+            spy1.received
+        Test.Unit.Assert.equal left right
+      Test.Unit.test "distributes map: f <$> (x <|> y) == (f <$> x) <|> (f <$> y)" do
+        let
+          f :: Int -> Int
+          f = (+) 1
+
+          pulses ::
+            { s1 :: FRP.SinkEvent Int, s2 :: FRP.SinkEvent Int } ->
+            Effect Unit
+          pulses { s1, s2 } = do
+            s1.push 0
+            s2.push 1
+        left <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            s2 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              combined :: FRP.Event Int
+              combined = f <$> (s1.event <|> s2.event)
+            spy1 <- subscribeSpy combined
+            pulses { s1, s2 }
+            spy1.received
+        right <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            s2 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              combined :: FRP.Event Int
+              combined = (f <$> s2.event) <|> (f <$> s1.event)
+            spy1 <- subscribeSpy combined
+            pulses { s1, s2 }
+            spy1.received
+        Test.Unit.Assert.equal left right
+      Test.Unit.test "obeys commutative law: x <|> y = y <|> x" do
+        let
+          pulses ::
+            { s1 :: FRP.SinkEvent Int, s2 :: FRP.SinkEvent Int } ->
+            Effect Unit
+          pulses { s1, s2 } = do
+            s1.push 0
+            s2.push 1
+        left <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            s2 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              combined :: FRP.Event Int
+              combined = s1.event <|> s2.event
+            spy1 <- subscribeSpy combined
+            pulses { s1, s2 }
+            spy1.received
+        right <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            s2 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              combined :: FRP.Event Int
+              combined = s2.event <|> s1.event
+            spy1 <- subscribeSpy combined
+            pulses { s1, s2 }
+            spy1.received
+        Test.Unit.Assert.equal left right
+    Test.Unit.suite "empty (Plus)" do
+      Test.Unit.test "should never pulse" do
+        actual <-
+          liftEffect do
+            let
+              never :: FRP.Event Int
+              never = Control.Plus.empty
+            spy1 <- subscribeSpy never
+            spy1.received
+        Test.Unit.Assert.equal [] actual
+    Test.Unit.suite "map (Functor)" do
+      Test.Unit.test "should transform the published values" do
+        actual <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              transformed :: FRP.Event Int
+              transformed = map (_ + 1) s1.event
+            spy1 <- subscribeSpy transformed
+            Data.Traversable.for_ [ 0, 1, 2 ] s1.push
+            spy1.received
+        Test.Unit.Assert.equal [ 1, 2, 3 ] actual
+      Test.Unit.test "obeys identity law: map identity = identity" do
+        left <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              transformed :: FRP.Event Int
+              transformed = map identity s1.event
+            spy1 <- subscribeSpy transformed
+            Data.Traversable.for_ [ 0, 1, 2 ] s1.push
+            spy1.received
+        right <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              transformed :: FRP.Event Int
+              transformed = identity s1.event
+            spy1 <- subscribeSpy transformed
+            Data.Traversable.for_ [ 0, 1, 2 ] s1.push
+            spy1.received
+        Test.Unit.Assert.equal left right
+      Test.Unit.test "distributes over compose: map (g <<< f) = map g <<< map f" do
+        let
+          f :: Int -> Int
+          f = (+) 1
+
+          g :: Int -> String
+          g = show
+        left <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              transformed :: FRP.Event String
+              transformed = map (g <<< f) s1.event
+            spy1 <- subscribeSpy transformed
+            Data.Traversable.for_ [ 0, 1, 2 ] s1.push
+            spy1.received
+        right <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              transformed :: FRP.Event String
+              transformed = (map g <<< map f) s1.event
+            spy1 <- subscribeSpy transformed
+            Data.Traversable.for_ [ 0, 1, 2 ] s1.push
+            spy1.received
+        Test.Unit.Assert.equal left right
+    Test.Unit.suite "filter" do
+      Test.Unit.test "should remove unwanted pulses" do
+        actual <-
+          liftEffect do
+            s1 :: FRP.SinkEvent Int <- FRP.sinkEvent
+            let
+              predicate :: Int -> Boolean
+              predicate x = x `mod` 2 == 0
+
+              filtered :: FRP.Event Int
+              filtered = FRP.filter predicate s1.event
+            spy1 <- subscribeSpy filtered
+            Data.Traversable.for_ (Data.Array.range 0 9) s1.push
+            spy1.received
+        Test.Unit.Assert.equal [ 0, 2, 4, 6, 8 ] actual
 
 subscribeSpy ::
   forall a.
