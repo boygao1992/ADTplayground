4f19ccb9f15207c03ff73aab130a9c21ee003774
boygao1992 <wg2313@columbia.edu>
2020-08-05 00:37:11 -0400

    Phaser.Input.Pointer.getInterpolatedPosition

--- purescript-aim/src/Phaser/Input/Pointer.js
+++ purescript-aim/src/Phaser/Input/Pointer.js
@@ -1,5 +1,9 @@
 "use strict";
 
+exports._getInterpolatedPosition = function getInterpolatedPosition ( { pointer, steps } ) {
+  return pointer.getInterpolatedPosition( steps );
+}
+
 exports._point = function point( pointer ) {
   return Object.freeze( { x: pointer.x, y: pointer.y } );
 }
--- purescript-aim/src/Phaser/Input/Pointer.purs
+++ purescript-aim/src/Phaser/Input/Pointer.purs
@@ -1,14 +1,44 @@
 module Phaser.Input.Pointer
   ( Pointer
+  , getInterpolatedPosition
   , point
   ) where
 
 import Data.Function.Uncurried as Data.Function.Uncurried
+import Effect (Effect)
+import Effect.Uncurried as Effect.Uncurried
 
 -----------------------
 -- Phaser.Input.Pointer
 -----------------------
 foreign import data Pointer :: Type
+
+foreign import _getInterpolatedPosition ::
+  Effect.Uncurried.EffectFn1
+    { pointer :: Pointer
+    , steps :: Int
+    }
+    ( Array
+        { x :: Number
+        , y :: Number
+        }
+    )
+
+getInterpolatedPosition ::
+  Pointer ->
+  Int ->
+  Effect
+    ( Array
+        { x :: Number
+        , y :: Number
+        }
+    )
+getInterpolatedPosition pointer steps =
+  Effect.Uncurried.runEffectFn1
+    _getInterpolatedPosition
+    { pointer
+    , steps
+    }
 
 foreign import _point ::
   Data.Function.Uncurried.Fn1
